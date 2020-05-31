d7ed9a3515a291fef1278349920f430f1a14d01c
rajanjamu <rajanjamu@gmail.com>
2020-05-31 16:27:10 +0530

    public dir added

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express'),
       router    = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('This is the root route!')
+    res.sendFile('index.html')
 })
 
 module.exports = router
